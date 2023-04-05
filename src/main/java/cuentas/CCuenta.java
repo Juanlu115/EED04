b9cfee884c929371a80e31b05d29c71adc48825c
Juan Luis <juanluisgns@gmail.com>
2023-04-05 13:42:03 +0200

    Comentarios JavaDoc

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,53 +1,79 @@
 package cuentas;
 
-
 /**
  *
- * @author st_ju
+ * @author Juan Luis Garcia-Nieto Solis
  */
 public class CCuenta {
-
-
-
+    
+    //Atributos
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
+    
+    //Constructores
 
-    public CCuenta()
-    {
+    /**
+     *
+     */
+
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    /**
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
+    
+    //Metodos
 
-    public double estado()
-    {
+    /**
+     *
+     * @return
+     */
+
+    public double estado() {
         return getSaldo();
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    /**
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
-    
-        /**
+
+    /**
      * @return the nombre
      */
     public String getNombre() {
